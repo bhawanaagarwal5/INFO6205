1887b332406fb94b705044276faa3fd0835cacc2
Bhawana Agarwal <agarwal.bh@northeastern.edu>
2022-02-12 00:21:25 -0500

    changes for timer function

--- src/main/java/edu/neu/coe/info6205/util/Timer.java
+++ src/main/java/edu/neu/coe/info6205/util/Timer.java
@@ -1,5 +1,6 @@
 package edu.neu.coe.info6205.util;
 
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -54,8 +55,26 @@ public class Timer {
      */
     public <T, U> double repeat(int n, Supplier<T> supplier, Function<T, U> function, UnaryOperator<T> preFunction, Consumer<U> postFunction) {
         logger.trace("repeat: with " + n + " runs");
+        for(int i=0;i<n;i++) {
+            pause();
+            T t = supplier.get();
+            if (preFunction!=null) {
+                t=preFunction.apply(t);
+            }
+                resume();
+            U u = function.apply(t);
+            pauseAndLap();
+            if(postFunction!=null) {
+                postFunction.accept(u);
+            }
+            resume();
+
+        }
+        laps--;
+        return stop();
+        
         // FIXME: note that the timer is running when this method is called and should still be running when it returns. by replacing the following code
-         return 0;
+         
         // END 
     }
 
@@ -175,7 +194,9 @@ public class Timer {
      */
     private static long getClock() {
         // FIXME by replacing the following code
-         return 0;
+        long getSysTime = System.nanoTime();
+        
+        return getSysTime;
         // END 
     }
 
@@ -188,7 +209,9 @@ public class Timer {
      */
     private static double toMillisecs(long ticks) {
         // FIXME by replacing the following code
-         return 0;
+        
+//        return TimeUnit.MILLISECONDS.convert(ticks, TimeUnit.NANOSECONDS);
+        return Math.max(0L, Math.round(ticks / 1e6));
         // END 
     }
 
