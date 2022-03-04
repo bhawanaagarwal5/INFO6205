924e752682aa214d8d103beee0e6d3ee34ef4902
Bhawana Agarwal <agarwal.bh@northeastern.edu>
2022-03-04 01:46:31 -0500

    initial commit

--- src/main/java/edu/neu/coe/info6205/union_find/HWQUPC_Solution.java
+++ src/main/java/edu/neu/coe/info6205/union_find/HWQUPC_Solution.java
@@ -1,4 +1,54 @@
 package edu.neu.coe.info6205.union_find;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class HWQUPC_Solution {
+    
+    
+    
+    public static void executeUFwithPC(int num){
+        UF u = new UF_HWQUPC(num);
+        Random r = new Random();
+        
+        int connectedPairs = 0;
+        while(u.components() != 1){
+            int i = r.nextInt(num);
+            int j = r.nextInt(num);
+            
+            if ( i == j) continue;
+            
+            if(! u.isConnected(i, j)){
+//                System.out.println("Connecting...");
+                u.connect(i, j);
+                
+            }
+//            else {
+//                System.out.println("Elements are already connected!");
+//            }
+
+            connectedPairs++;
+            
+            
+        }
+        System.out.println("Number of elements/sites : " + num);
+        System.out.println("Number of connections : " + connectedPairs);
+        
+        System.out.println("====================================================");
+    }
+    
+    public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+        int numOfSites = sc.nextInt();
+        System.out.println("Number of sites are : " + numOfSites);
+        System.out.print("Enter the number of experiments : ");
+        int exp = sc.nextInt();
+        sc.close();
+        for (int i = 0; i < exp; i++ ){
+            if(i!=0){
+                numOfSites *= 2;
+            }
+            executeUFwithPC(numOfSites);
+        }
+    }
 }
